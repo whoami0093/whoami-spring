a49697c28a638fdc0581c19f6b3324327ed078db
Anton Tamarov <atamarov@icerockdev.com>
2024-12-15 16:19:51 +0600

    Init project

---
+++ src/test/kotlin/com/whoami/whoamiSpring/WhoamiSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.whoami.whoamiSpring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhoamiSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
